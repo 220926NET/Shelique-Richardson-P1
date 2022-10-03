e687c026c8d87229ee7a6fda49bd62074b3fdf3c
Shelique <81508244+sheshe2795@users.noreply.github.com>
2022-10-03 12:00:25 -0400

    MVP list

--- Project-1-ERS/Program.cs
+++ Project-1-ERS/Program.cs
@@ -1,2 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
+/*
+MVPs:
+-Users should be able to login/register
+-Users should have either Employee or Manager role
+-Users should be able to submit expense reimbrsement ticket
+-Managers should be able to process tickets
+-Employees should be able to vew all prior ticket submissions
+--Database connection-SQL
+
+Ideas~
+--Employees class with Manager class embedded with additional features (Inheritance)
+
+*/
+
+
 Console.WriteLine("Hello, World!");
